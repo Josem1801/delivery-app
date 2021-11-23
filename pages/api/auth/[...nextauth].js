394babf908ca55b91cb03111987b4f522ca7113b
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2021-11-23 09:49:38 -0600

    fixing next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
-export default NextAuth({
+const options = {
   // https://next-auth.js.org/configuration/providers
   providers: [
     GoogleProvider({
@@ -15,4 +15,23 @@ export default NextAuth({
     signIn: "/account/login",
     signOut: "/account",
   },
-});
+};
+
+export default function (req, res) {
+  setNextAuthUrl(req);
+  NextAuth(req, res, options);
+}
+function setNextAuthUrl(req) {
+  const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
+  const host = req.headers["host"];
+
+  if (!host) {
+    throw new Error(
+      `The request has no host header which breaks authentication and authorization.`
+    );
+  }
+
+  process.env.NEXTAUTH_URL = `${protocol}://${host}`;
+
+  console.log("set envar NEXTAUTH_URL=%s", process.env.NEXTAUTH_URL);
+}
