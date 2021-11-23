3bd9078cdeef477bba40bed8e284b511b23aa617
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2021-11-23 10:28:54 -0600

    before use next-firebase-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
-const options = {
+export default NextAuth({
   // https://next-auth.js.org/configuration/providers
   providers: [
     GoogleProvider({
@@ -15,23 +15,4 @@ const options = {
     signIn: "/account/login",
     signOut: "/account",
   },
-};
-
-export default function (req, res) {
-  setNextAuthUrl(req);
-  NextAuth(req, res, options);
-}
-function setNextAuthUrl(req) {
-  const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
-  const host = req.headers["host"];
-
-  if (!host) {
-    throw new Error(
-      `The request has no host header which breaks authentication and authorization.`
-    );
-  }
-
-  process.env.NEXTAUTH_URL = `${protocol}://${host}`;
-
-  console.log("set envar NEXTAUTH_URL=%s", process.env.NEXTAUTH_URL);
-}
+});
