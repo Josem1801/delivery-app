a3b2ea4d33ab2b17dd9006efc4cb2907efadad1d
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2021-11-24 15:47:29 -0600

    give access

--- initAuth.js
+++ initAuth.js
@@ -14,7 +14,7 @@ const initAuth = () => {
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
         // The private key must not be accesssible on the client side.
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
       },
       databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     },
