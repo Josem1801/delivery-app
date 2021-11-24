8d604012f20dc29c44b14e3d9fc97e7323ab7477
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2021-11-24 14:31:22 -0600

    Authentication complete :)

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import "../styles/globals.css";
 
 import initAuth from "../initAuth"; // the module you created above
+import { withAuthUser } from "next-firebase-auth";
 
 initAuth();
 function MyApp({ Component, pageProps: { ...pageProps } }) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default withAuthUser()(MyApp);
