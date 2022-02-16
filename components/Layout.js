923f707ef3b10a2800642dac224308109b671bc9
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2022-02-16 15:51:47 -0600

    Fix metadata

--- components/Layout.js
+++ components/Layout.js
@@ -46,9 +46,8 @@ function Layout({
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="description of your project" />
+        <meta name="description" content="Order your favorite food" />
         <meta name="theme-color" content="#000" />
-        <title>Title of the project</title>
         <link rel="manifest" href="/manifest.json" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
